39250af1ef49f726e8f0910bd798be21efec8633
danmuresan <dan91142006@yahoo.com>
2020-02-10 23:51:49 +0100

    Add some more binary tree problems

--- src/main/kotlin/TreeProblems.kt
+++ src/main/kotlin/TreeProblems.kt
@@ -22,8 +22,80 @@ object TreeProblems {
             return root.isBst()
         }
 
-        fun lowestCommonAncestor() {
-            // TODO: ...
+        fun doesNodeExistInTree(nodeToFind: String): Boolean {
+            fun findNode(currentNode: BinaryTreeNode?, nodeToFind: String): Boolean {
+                if (currentNode == null) {
+                    return false
+                }
+
+                if (currentNode.value == nodeToFind) {
+                    return true
+                }
+
+                val isNodeInLeftSubtree = findNode(currentNode.leftSubtree, nodeToFind)
+                val isNodeInRightSubtree = findNode(currentNode.rightSubtree, nodeToFind)
+                return isNodeInLeftSubtree || isNodeInRightSubtree
+            }
+
+            return findNode(root, nodeToFind)
+        }
+
+        fun findPathToNode(nodeToFind: String): List<BinaryTreeNode> {
+            fun findNodeRec(currentNode: BinaryTreeNode?, nodeToFind: String, path: MutableList<BinaryTreeNode>): Boolean {
+                // base case - we reached the end of the tree and found nothing
+                if (currentNode == null) {
+                    return false;
+                }
+
+                // base case - we found the node
+                if (currentNode.value == nodeToFind) {
+                    path.add(currentNode)
+                    return true
+                }
+
+                // go search leftSubtree and add the nodes
+                val isFoundInLeftSubTree = findNodeRec(currentNode.leftSubtree, nodeToFind, path)
+                if (isFoundInLeftSubTree) {
+                    path.add(currentNode)
+                    return true
+                }
+
+                // go search rightSubtree and add the nodes
+                val isFoundInRightSubTree = findNodeRec(currentNode.rightSubtree, nodeToFind, path)
+                if (isFoundInRightSubTree) {
+                    path.add(currentNode)
+                    return true
+                }
+
+                return false
+            }
+
+            val path = mutableListOf<BinaryTreeNode>()
+            findNodeRec(root, nodeToFind, path)
+            return path
+        }
+
+        // double traversal (there's an optimize, single traversal solution)
+        fun lowestCommonAncestor(node1: String, node2: String): String {
+            val pathToNode1 = findPathToNode(node1)
+            val pathToNode2 = findPathToNode(node2)
+
+            var lca = "Not Found"
+            val diff = Math.max(pathToNode1.size, pathToNode2.size) - Math.min(pathToNode1.size, pathToNode2.size)
+            var idx = 0
+            while (idx < Math.max(pathToNode1.size, pathToNode2.size)) {
+                if (pathToNode1[idx + diff] == pathToNode2[idx]) {
+                    return pathToNode2[idx].value
+                }
+
+                idx++
+            }
+
+            return lca
+        }
+
+        fun findMinPathBetweenNodes(node1: String, node2: String): List<BinaryTreeNode> {
+            return listOf() // TODO: ....
         }
 
         fun getHeight(): Int {
@@ -116,5 +188,21 @@ object TreeProblems {
         sampleTreeBst.preorder()
         print(", postorder BST: ")
         sampleTreeBst.postorder()
+
+        print("\nFind if node g exists in sample tree 1: " + sampleTree.doesNodeExistInTree("g"))
+        print("\nFind if node x exists in sample tree 1: " + sampleTree.doesNodeExistInTree("x"))
+
+        val path = sampleTree.findPathToNode("g")
+        print("\nFind path to node g in sample tree 1: ")
+        path.print()
+
+        val lca = sampleTree.lowestCommonAncestor("g", "d")
+        print("\nFind LCA between node g and node d in sample tree 1: " + lca)
+    }
+
+    fun List<TreeProblems.BinaryTreeNode>.print() {
+        this.forEach {
+            print(it.value + ", ")
+        }
     }
 }
